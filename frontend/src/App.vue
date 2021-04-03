bb1b48040813e0d7f35a674da14ba3453dd1c95c
Shawn <45756873+shawn-p-m@users.noreply.github.com>
2021-04-03 16:08:14 -0700

    Added footer and removed register link from top

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -6,16 +6,27 @@
           <h1 class=" white--text">Vue Your Blog</h1>
         </router-link>
       </div>
-
       <v-spacer></v-spacer>
-      <v-btn text to="/register">
-        <span>Register</span>
-      </v-btn>
     </v-app-bar>
 
     <v-main>
       <router-view />
     </v-main>
+    <v-footer color="primary lighten-1" padless>
+      <v-row justify="center" no-gutters>
+        <v-btn
+          v-for="link in links"
+          :key="link"
+          color="white"
+          text
+          rounded
+          class="my-2"
+          :to="link.path"
+        >
+          {{ link.name }}
+        </v-btn>
+      </v-row>
+    </v-footer>
   </v-app>
 </template>
 
@@ -24,7 +35,10 @@ export default {
   name: 'App',
 
   data: () => ({
-    //
+    links: [
+      { path: '/', name: 'Home' },
+      { path: '/register', name: 'Register' },
+    ],
   }),
 }
 </script>
