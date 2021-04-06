469f0fe1aeecdb4e1921b8f2682f4ef47a4c7406
Shawn <45756873+shawn-p-m@users.noreply.github.com>
2021-04-06 12:19:15 -0700

    fixing vuelidate array issue

--- frontend/src/components/BlogForm.vue
+++ frontend/src/components/BlogForm.vue
@@ -15,19 +15,41 @@
           >
           </v-text-field>
           <div v-for="blogElement in blogElements" :key="blogElement.id">
-            <v-row class="d-flex align-center mx-1">
+            <v-row class="d-flex align-center mx-1 my-1">
               <v-text-field
                 class="mr-2"
                 v-if="blogElement.type == 'header'"
                 v-model="blogElement.content"
-                :error-messages="headerErrors"
+                :error-messages="headerErrors(blogElement.id)"
                 :counter="maxHeaderCharCount"
                 label="Header"
                 required
-                @input="$v.header.$touch()"
-                @blur="$v.header.$touch()"
+                @input="$v.blogElements.$touch()"
+                @blur="$v.blogElements.$touch()"
               >
               </v-text-field>
+              <v-textarea
+                v-if="blogElement.type == 'textArea'"
+                v-model="blogElement.content"
+                :error-messages="textAreaErrors"
+                :counter="maxTextAreaCharCount"
+                label="Text Area"
+                type="body"
+                required
+                @input="$v.textArea.$touch()"
+                @blur="$v.textArea.$touch()"
+              ></v-textarea>
+              <v-file-input
+                v-if="blogElement.type === 'image'"
+                v-model="blogElement.content"
+                :rules="imageRules"
+                :error-messages="imageErrors"
+                placeholder="Insert Pic"
+                accept="image/png, image/jpeg, image/bmp"
+                prepend-icon="mdi-camera"
+                label="Insert Profile Pic"
+                chips
+              />
               <v-btn
                 @click="removeBlogElement(blogElement.id)"
                 color="red"
@@ -40,29 +62,6 @@
                 </v-icon>
               </v-btn>
             </v-row>
-            <v-textarea
-              v-if="blogElement.type == 'textArea'"
-              v-model="body"
-              :error-messages="textAreaErrors"
-              :counter="maxCharCount"
-              label="Body"
-              type="body"
-              required
-              @input="$v.body.$touch()"
-              @blur="$v.body.$touch()"
-            ></v-textarea>
-
-            <v-file-input
-              v-if="blogElement.type === 'image'"
-              v-model="image"
-              :rules="imageRules"
-              :error-messages="imageErrors"
-              placeholder="Insert Pic"
-              accept="image/png, image/jpeg, image/bmp"
-              prepend-icon="mdi-camera"
-              label="Insert Profile Pic"
-              chips
-            ></v-file-input>
           </div>
           <v-row class="d-flex justify-end my-6 mx-1">
             <v-speed-dial
@@ -81,10 +80,22 @@
                   </v-icon>
                 </v-btn>
               </template>
-              <v-btn fab dark small color="orange">
+              <v-btn
+                @click="addBlogElement('image')"
+                fab
+                dark
+                small
+                color="orange"
+              >
                 <v-icon>mdi-image</v-icon>
               </v-btn>
-              <v-btn fab dark small color="blue">
+              <v-btn
+                @click="addBlogElement('textArea')"
+                fab
+                dark
+                small
+                color="blue"
+              >
                 <v-icon>mdi-text-box</v-icon>
               </v-btn>
               <v-btn
@@ -126,27 +137,34 @@
 <script>
 import { validationMixin } from 'vuelidate'
 import { required, maxLength, minLength } from 'vuelidate/lib/validators'
-import EventService from '@/services/EventService'
+// import EventService from '@/services/EventService'
 // import the component and the necessary extensions
 
 export default {
   mixins: [validationMixin],
   validations: {
     title: { required, maxLength: maxLength(14), minLength: minLength(4) },
-    header: { required, maxLength: maxLength(128), minLength: minLength(4) },
-    body: { required, maxLength: maxLength(2096), minLength: minLength(255) },
+    blogElements: {
+      $each: { required, maxLength: maxLength(128), minLength: minLength(4) },
+    },
+    textArea: {
+      required,
+      maxLength: maxLength(2096),
+      minLength: minLength(255),
+    },
     image: { required },
   },
 
   data: () => ({
     title: '',
-    body: '',
+    textArea: '',
     uniqueId: 0,
     blogElements: [],
     image: null,
     minCharCount: 4,
     maxCharCount: 14,
     maxHeaderCharCount: 128,
+    maxTextAreaCharCount: 4096,
     submitStatus: null,
     imageRules: [
       value =>
@@ -154,26 +172,10 @@ export default {
         value.size < 4000000 ||
         'Avatar size should be less than 4 MB!',
     ],
-    direction: 'top',
     fab: false,
-    fling: false,
-    tabs: null,
   }),
 
   computed: {
-    activeFab() {
-      switch (this.tabs) {
-        case 'one':
-          return { class: 'purple', icon: 'account_circle' }
-        case 'two':
-          return { class: 'red', icon: 'edit' }
-        case 'three':
-          return { class: 'green', icon: 'keyboard_arrow_up' }
-        default:
-          return {}
-      }
-    },
-
     titleErrors() {
       const errors = []
       if (!this.$v.title.$dirty) return errors
@@ -188,27 +190,48 @@ export default {
       !this.$v.title.required && errors.push('Title is required.')
       return errors
     },
-    headerErrors() {
+    headerErrors(blogElementId) {
+      let blogElementIndex = this.blogElements.findIndex(
+        blogElement => blogElement.id === blogElementId
+      )
+      console.log(blogElementIndex)
       const errors = []
-      if (!this.$v.header.$dirty) return errors
-      !this.$v.header.maxLength &&
+      if (!this.$v.blogElements.$each[blogElementIndex].$dirty) return errors
+      !this.$v.blogElements.$each[blogElementIndex].maxLength &&
         errors.push(
           'Header must be at most ' +
             this.maxHeaderCharCount +
             ' characters long'
         )
-      !this.$v.header.minLength &&
+      !this.$v.blogElements.$each[blogElementIndex].minLength &&
         errors.push(
           'Header must be at least ' + this.minCharCount + ' characters long'
         )
-      !this.$v.header.required && errors.push('Title is required.')
+      !this.$v.blogElements.$each[blogElementIndex].required &&
+        errors.push('Header is required.')
+      return errors
+    },
+    textAreaErrors() {
+      const errors = []
+      if (!this.$v.textArea.$dirty) return errors
+      !this.$v.textArea.maxLength &&
+        errors.push(
+          'Text Area must be at most ' +
+            this.maxTextAreaCharCount +
+            ' characters long'
+        )
+      !this.$v.textArea.minLength &&
+        errors.push(
+          'Text Area must be at least ' + this.minCharCount + ' characters long'
+        )
+      !this.$v.textArea.required && errors.push('TextArea is required.')
       return errors
     },
 
     imageErrors() {
       const errors = []
       if (!this.$v.image.$dirty) return errors
-      !this.$v.image.required && errors.push('Profile Pic is required')
+      !this.$v.image.required && errors.push('Profile Pic is required.')
       return errors
     },
   },
@@ -230,45 +253,30 @@ export default {
       this.blogElements.splice(blogElementIndex, 1)
     },
     submit() {
-      var bodyFormData = new FormData()
-      bodyFormData.append('title', this.title)
-      bodyFormData.append('body', this.body)
-      bodyFormData.append('image', this.image)
-      this.$v.$touch()
-      if (this.$v.$invalid) {
-        this.submitStatus = 'ERROR'
-      } else {
-        // do your submit logic here
-        this.submitStatus = 'PENDING'
-        EventService.createUser(bodyFormData)
-          .then(response => {
-            console.log(response)
-            this.submitStatus = 'OK'
-          })
-          .then(() => {
-            this.$router.push('/')
-          })
-      }
+      // var bodyFormData = new FormData()
+      // bodyFormData.append('title', this.title)
+      // bodyFormData.append('body', this.body)
+      // bodyFormData.append('image', this.image)
+      // this.$v.$touch()
+      // if (this.$v.$invalid) {
+      //   this.submitStatus = 'ERROR'
+      // } else {
+      //   // do your submit logic here
+      //   this.submitStatus = 'PENDING'
+      //   EventService.createUser(bodyFormData)
+      //     .then(response => {
+      //       console.log(response)
+      //       this.submitStatus = 'OK'
+      //     })
+      //     .then(() => {
+      //       this.$router.push('/')
+      //     })
+      // }
     },
     clear() {
       this.$v.$reset()
       this.title = ''
       this.body = ''
-      this.checkbox = false
-    },
-  },
-  watch: {
-    top(val) {
-      this.bottom = !val
-    },
-    right(val) {
-      this.left = !val
-    },
-    bottom(val) {
-      this.top = !val
-    },
-    left(val) {
-      this.right = !val
     },
   },
 }
